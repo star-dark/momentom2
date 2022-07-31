d1672ea4af84a9eddbc579d9c1be7d83d5c53244
star-dark <den1116@naver.com>
2022-07-31 18:20:11 +0900

    greeting

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,18 @@
-const 
+const loginForm = document.getElementById("login-form");
+const getUserName = document.querySelector("#login-form input");
+const UsernameKey ="username";
+const greetingText = document.getElementById("greeting");
+
+function loginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add("hidden");
+    const loginUser = getUserName.value;
+    localStorage.setItem(UsernameKey,loginUser);
+    greeting(loginUser);
+}
+function greeting(username) {
+    greetingText.innerText = `Hello ${username}!`;
+    greetingText.classList.remove("hidden");
+}
+const savedUsername = localStorage.getItem(UsernameKey);
+loginForm.addEventListener("submit", loginSubmit);
